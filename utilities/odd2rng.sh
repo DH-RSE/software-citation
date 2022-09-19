4ff16580f02c3b9cfb5a11895159acb05fb57b5a
Anne Ferger <anne.ferger@uni-paderborn.de>
2022-09-19 15:34:52 +0200

    rng conversion of odd file

--- utilities/odd2rng.sh
+++ utilities/odd2rng.sh
@@ -1,10 +1,1 @@
-folder=$1
-
-for file in $(ls ${folder}/*.odd);
-do 
-    [ -f "$file" ] || break
-    echo Processing ${file}
-    s=$(basename $file .odd)
-    #echo Output ${folder}/${s}.txt
-curl -o "${folder}/${s}.rng" -F upload=@"${file}" 'https://teigarage.tei-c.de/ege-webservice/Conversions/ODD%3Atext%3Axml/ODDC%3Atext%3Axml/relaxng%3Aapplication%3Axml-relaxng'
-done
+curl -o schema/tei_software_annotation.rng -F upload=@schema/tei_software_annotation.xml 'https://teigarage.tei-c.de/ege-webservice/Conversions/ODD%3Atext%3Axml/ODDC%3Atext%3Axml/relaxng%3Aapplication%3Axml-relaxng' 
