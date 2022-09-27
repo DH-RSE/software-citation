1472a3b6e262f45340ae7db9b718f6ab881a6d41
Anne Ferger <anne.ferger@uni-paderborn.de>
2022-09-27 11:30:08 +0200

    debuggin github action

--- utilities/odd2oddSoftwareList.sh
+++ utilities/odd2oddSoftwareList.sh
@@ -1,5 +1,6 @@
 curl -s -L -o /tmp/saxon.zip https://sourceforge.net/projects/saxon/files/Saxon-HE/11/Java/SaxonHE11-4J.zip/download
-unzip /tmp/saxon.zip -d /tmp/saxon 
-rm /tmp/saxon.zip 
-cp -r /tmp/saxon/saxon-he-11.4.jar  saxon-he.jar 
-java -jar 'saxon-he.jar' -xsl:'utilities/addSoftwareList2Odd.xsl' -s:'schema/tei_software_annotation.xml' -o:'schema/tei_software_annotation.xml'
+unzip /tmp/saxon.zip -d /tmp/saxon
+rm /tmp/saxon.zip
+echo ls /tmp
+cp -r /tmp/saxon/saxon-he-11.4.jar /tmp/saxon-he.jar 
+java -jar '/tmp/saxon-he.jar' -xsl:'utilities/addSoftwareList2Odd.xsl' -s:'schema/tei_software_annotation.xml' -o:'schema/tei_software_annotation.xml'
